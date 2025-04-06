e6dea8db32dadef7f5e08787745c60d3e0934446
jaspyplayz <jaspyplayz@gmail.com>
2025-04-06 10:48:58 -0400

    -Initialized constants.py -Initizalied game.py -Initialized main.py -Initialized player.py -Initialized utils/helper.py

---
+++ constants.py
@@ -0,0 +1,18 @@
+#Initialize Display
+screen_width = 800
+screen_height = 600
+FPS = 60
+TITLE = "WASD Movement Game"
+
+#Player initalization and frame of reference
+PLAYER_SIZE = 50
+player_pos = [screen_width // 2, screen_height - 2 * player_size]
+PLAYER_SPEED = 5     
+
+#Preset colors
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+
+
